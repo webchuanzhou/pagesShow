f444fee5356a05dc694b0f9d915606f4cf1610e1
webchuanzhou <webchuanzhou@163.com>
2021-06-15 18:33:55 +0800

    build

---
+++ assets/About.df883154.js
@@ -0,0 +1,1 @@
+import{o as a,c as o,g as s}from"./vendor.9b61ed1f.js";const e={},n={class:"about"},r=s("h1",null,"This is an about page",-1);e.render=function(s,e){return a(),o("div",n,[r])};export default e;
